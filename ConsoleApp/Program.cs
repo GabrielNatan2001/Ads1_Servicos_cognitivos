08009bfda42fd9e48b2e814471d9202cba00b4a9
gabriel <gabrielnatan.2001.pereira@gmail.com>
2024-09-06 11:16:29 -0300

    adicionando leitura de arquivo para labirinto

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Digite o caminho do arquivo:");
+
+        string caminhoArquivo = Console.ReadLine();
+        List<char[]> labirinto = new List<char[]>();
+
+        try
+        {
+            using (StreamReader sr = new StreamReader(caminhoArquivo))
+            {
+                string linha;
+                while ((linha = sr.ReadLine()) != null)
+                {
+                    labirinto.Add(linha.ToCharArray());
+                }
+            }
+
+            // Exibe a matriz de labirinto
+            Console.WriteLine("Labirinto:");
+            foreach (char[] linha in labirinto)
+            {
+                Console.WriteLine(new string(linha));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ocorreu um erro: " + e.Message);
+        }
+    }
+}
