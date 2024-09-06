f6a2844bb6b099bbea4ba2fbd7047100b5eb490f
gabriel <gabrielnatan.2001.pereira@gmail.com>
2024-09-06 11:27:32 -0300

    Criando classe de robo

---
+++ ConsoleApp/Robo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Robo
+    {
+        public int[,] Posicao { get; set; }
+        public bool EncontrouHumano { get; set; } = false;
+
+        public void MoverParaFrente()
+        {
+
+        }
+        public void MoverParaDireta()
+        {
+
+        }
+        public void PegarHumano()
+        {
+
+        }
+        public void EjetarHumano()
+        {
+
+        }
+        public void Log(string movimento)
+        {
+        }
+    }
+}
